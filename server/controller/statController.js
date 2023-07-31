921b7050d32347e6d1095ef93293deb2268908ce
Daniel Zhao <dzhao1880@gmail.com>
2023-07-31 16:18:42 -0400

    initialFectch feature

--- server/controller/statController.js
+++ server/controller/statController.js
@@ -13,10 +13,10 @@ statController.initialFetch = async (req, res, next) => {
       console.log("empty");
       let insertQuery = `INSERT INTO Users (username,sent, phoneInter, interview, offer,rejections, noReply) VALUES($1,0,0,0,0,0,0)`;
       resp = await db.query(insertQuery, param);
-      console.log("new user", resp);
+      //   console.log("new user", resp);
     }
     res.locals.user = resp;
-    console.log(resp);
+    // console.log(resp);
     return next();
   } catch (error) {
     return error;
