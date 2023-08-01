8a6aae9049ce5064f4a49a0c9b86fc0db5b32a72
Vincent Do <vincentd4992@gmail.com>
2023-08-01 11:59:21 -0400

    Access to code returned from OAuth

--- client/components/Stats.jsx
+++ client/components/Stats.jsx
@@ -1,7 +1,16 @@
-import React from "react"
+import React, {useEffect} from "react"
 
 
 const Stats = () => {
+    useEffect(()=>{
+        const queryString = window.location.search
+        const urlParams = new URLSearchParams(queryString);
+        const codeParam = urlParams.get('code')
+        console.log(codeParam)
+
+        
+    },[]);
+
     return(
         <div>
             <p>Testing stat page</p>
--- client/components/login.jsx
+++ client/components/login.jsx
@@ -1,13 +1,19 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 const CLIENT_ID = process.env.GITHUB_CLIENT_ID
 
-function auth (){
-    window.location.assign('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID + '&redirect_uri=http://localhost:8080/stats')
 
-}
 
+ 
 const Login = ()=>{
+    
+   
+    
+    
+    function auth (){
+        window.location.assign('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID + '&redirect_uri=http://localhost:8080/stats')
+    }
+
     return (
         <div>
             <button onClick={auth}>OAuth for Github</button>
